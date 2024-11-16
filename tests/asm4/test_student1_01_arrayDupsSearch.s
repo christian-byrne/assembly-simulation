9f83172a85e5c55715984b297b2e7129cb17136f
christian-byrne <abolkonsky.rem@gmail.com>
2024-11-15 18:22:41 -0700

    Add tests

---
+++ tests/asm4/test_student1_01_arrayDupsSearch.s
@@ -0,0 +1,26 @@
+.data
+array:
+
+.text
+
+.globl main
+main:
+    la      $a0, array
+    addi    $a1, $zero, 0
+    addi    $a2, $zero, 2
+    jal     arrayDupSearchInts
+    
+    addi    $a0, $v0, 0    # make print int print result of arrayDupSearchInts
+    addi    $v0, $zero, 1
+    
+    syscall
+    
+    addi    $v0, $zero, 11
+    addi    $a0, $zero, 0xa
+    syscall  # the grading script expects extra newline after output of tests
+    
+    # apparently, it's necessary to syscall exit to make sure MARS doesn't 
+    # infinitely run the test.
+    
+    addi  $v0, $zero,10
+    syscall
---
+++ tests/asm4/test_student1_37_substringSearch5.s
@@ -0,0 +1,34 @@
+.data
+teststring:    .asciiz   "fedcbabcd"
+.text
+
+.globl main
+main:
+    la    $a0, teststring
+    addi  $a1, $zero, 'a'
+    addi  $a2, $zero, 'b'
+    addi  $a3, $zero, 'c'
+    
+    addi  $t0, $zero, 'd'
+    addi  $t1, $zero, 'e'
+    
+    sw    $t0, -8($sp)
+    sw    $t1, -4($sp)
+    
+
+    jal   stringSearch5
+    
+    addi    $a0, $v0, 0    # make print int print result of stringSearch5
+    addi    $v0, $zero, 1
+    
+    syscall
+    
+    addi    $v0, $zero, 11
+    addi    $a0, $zero, 0xa
+    syscall  # the grading script expects extra newline after output of tests
+    
+    # apparently, it's necessary to syscall exit to make sure MARS doesn't 
+    # infinitely run the test.
+    
+    addi  $v0, $zero,10
+    syscall
